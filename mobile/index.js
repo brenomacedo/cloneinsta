e966fcafe659090559be3b8e07fb34101ece04d1
brenosekiro <b88660501@gmail.com>
2020-04-28 23:07:29 -0300

    fireBaseConfig

--- mobile/index.js
+++ mobile/index.js
@@ -4,6 +4,9 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import axios from 'axios'
+
+
 import React from 'react'
 import { Provider } from 'react-redux'
 import storeConfig from './src/store/storeConfig'
--- mobile/src/store/actions/posts.js
+++ mobile/src/store/actions/posts.js
@@ -1,10 +1,18 @@
 import { ADD_POST, ADD_COMMENT } from '../actionTypes'
+import axios from 'axios'
 
 export const addPost = post => {
-    return {
-        type: ADD_POST,
-        payload: post
+
+    return dispatch => {
+        axios.post('https://cloneinstagram-afa2f.firebaseio.com/posts.json', {
+            ...post
+        }).then(res => console.log(res)).catch(err => console.log(err))
     }
+
+    // return {
+    //     type: ADD_POST,
+    //     payload: post
+    // }
 }
 
 export const addComment = comment => {
